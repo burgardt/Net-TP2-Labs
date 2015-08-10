d84c8eda4601bf61b606e75a3fa38d2ecb4abb1c
Maurito <mauro88rc@gmail.com>
2015-08-10 17:33:42 -0300

    Lab 3

---
+++ TP2/Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
+
+namespace Business.Logic
+{
+    public class UsuarioLogic: BusinessLogic
+    {
+        UsuarioAdapter _usuarioData;
+
+        public UsuarioAdapter UsuarioData
+        {
+            get { return _usuarioData; }
+            set { _usuarioData = value; }
+        }
+
+        public Usuario GetOne(int ID)
+        {
+            return UsuarioData.GetOne(ID);
+        }
+
+        public List<Usuario> GetAll()
+        {
+            return UsuarioData.GetAll();
+        }
+
+        public void Delete(int ID)
+        {
+            UsuarioData.Delete(ID);
+        }
+
+        public void Save(Usuario usuario)
+        {
+            UsuarioData.Save(usuario);
+        }
+    }
+}
